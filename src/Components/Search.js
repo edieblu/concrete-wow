392932e3f081f8f8f4fe3927100990f4a8f41fa3
EVA <edie.blues@gmail.com>
2020-04-25 22:19:04 +0200

    more error handling

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,49 +1,50 @@
 import React, { useState, useCallback } from "react";
-import Answer from './Answer';
-import axios from 'axios';
-
+import Answer from "./Answer";
+import axios from "axios";
 
 export default function Search() {
-  const BASE_URL = "https://api.factually.dev/reputation?"
+  const BASE_URL = "https://api.factually.dev/reputation?";
   const [searchTerm, setSearchTerm] = useState("");
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState(null);
-  const [isError, setIsError] = useState(false)
-  const [isValidUrl, setIsValidUrl] = useState(true)
+  const [isError, setIsError] = useState(false);
+  const [isValidUrl, setIsValidUrl] = useState(true);
 
-  const validUrlRegex = new RegExp('(^http[s]?:\/{2})|(^www)|(^\/{1,2})')
-
+  const validUrlRegex = new RegExp("(^http[s]?:/{2})|(^www)|(^/{1,2})");
 
   const _sendSerchRequest = useCallback(async () => {
-    const url = `${BASE_URL}url=${searchTerm}`
+    const url = `${BASE_URL}url=${searchTerm}`;
     if (validUrlRegex.test(searchTerm)) {
-      setIsValidUrl(true)
-      setIsLoading(true)
-      setData(null)
-      setIsError(false)
+      setIsLoading(true);
+      setData(null);
+      setIsError(false);
       try {
         const result = await axios(url);
         setData(result.data);
         setSearchTerm("");
       } catch (error) {
         setIsError(true);
-        setData(null)
+        setData(null);
       }
-      setIsLoading(false)
-    }
-    else setIsValidUrl(false)
+      setIsLoading(false);
+    } else setIsValidUrl(false);
+  }, [searchTerm, isLoading, isError, data, isValidUrl]);
 
-  }, [searchTerm, isLoading, isError, data]);
+  const _checkKeyActions = useCallback(
+    (event) => {
+      if (event.key === "Escape") {
+        event.preventDefault();
+        setSearchTerm("");
+        setIsValidUrl(true);
+      } else if (event.key === "Enter") {
+        event.preventDefault();
+        setIsValidUrl(true);
 
-  const _checkKeyActions = useCallback((event) => {
-    if (event.key === "Escape") {
-      event.preventDefault();
-      setSearchTerm("");
-    } else if (event.key === "Enter") {
-      event.preventDefault();
-      _sendSerchRequest();
-    }
-  }, [searchTerm]);
+        _sendSerchRequest();
+      }
+    },
+    [searchTerm]
+  );
 
   const _updateSearchTerm = useCallback(
     (event) => setSearchTerm(event.target.value),
@@ -51,34 +52,49 @@ export default function Search() {
   );
 
   return (
-    <div className='search-wrapper'>
-    <div className="search">
-      <input
-        type="text"
-        className="search-term"
-        value={searchTerm}
-        onChange={_updateSearchTerm}
-        onKeyDown={_checkKeyActions}
-        placeholder="https://"
-        autoFocus
-      />
-      {isLoading ? <div className='loading'>
-      <div className='spinner'></div>
+    <div className="search-wrapper">
+      <div className="search">
+        <input
+          type="text"
+          className="search-term"
+          value={searchTerm}
+          onChange={_updateSearchTerm}
+          onKeyDown={_checkKeyActions}
+          placeholder="https://"
+          autoFocus
+        />
+        {isLoading ? (
+          <div className="loading">
+            <div className="spinner"></div>
+          </div>
+        ) : (
+          <button
+            type="submit"
+            className="search-button"
+            onClick={_sendSerchRequest}
+          >
+            <i className="fa fa-search"></i>
+          </button>
+        )}
       </div>
-      :
-      <button
-        type="submit"
-        className="search-button"
-        onClick={_sendSerchRequest}
-      >
-        <i className="fa fa-search"></i>
-      </button>}
-    </div>
-    <div>
-    {isError ? <p>Hmm 🤔. Something went wrong, please try again.</p> : null}
-    {!isValidUrl ? <p>Hmm 🤔. That doesn't look like a valid website URL. <br/>Make sure it start with 'www' or 'https'. Please try again.</p> : null}
-    {data !== null ? <Answer website={data.basisURL} isTrusted={data.safe} score={data.distance} /> : null}
-    </div>
+      <div>
+        {isError ? (
+          <p>Hmm 🤔. Something went wrong, please try again.</p>
+        ) : null}
+        {!isValidUrl ? (
+          <p>
+            Hmm 🤔. That doesn't look like a valid website URL. <br />
+            Make sure it start with 'www' or 'https'. Please try again.
+          </p>
+        ) : null}
+        {data !== null ? (
+          <Answer
+            website={data.basisURL}
+            isTrusted={data.safe}
+            score={data.distance}
+          />
+        ) : null}
+      </div>
     </div>
   );
 }
