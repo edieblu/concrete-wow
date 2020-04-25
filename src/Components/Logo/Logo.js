2db1249d1be0d5f3aebc5439498cdb2a21b7d348
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-04-25 11:44:38 +0200

    create Logo component

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Logo() {
+  return (
+    <img className="" style={{ margin: '1rem' }} src="/icons-images/dummy-logo.png" alt="logo" />
+  );
+}
+
+export default Logo;
