e8fde158e2629b9591be3ae28e0f89708d1214de
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-04-25 13:58:54 +0200

    add Footer Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Logo from './Components/Logo/Logo';
+import Footer from './Components/Footer/Footer';
 import './App.css';
 
 function App() {
@@ -8,24 +9,27 @@ function App() {
     alert('Hey it works a little bit 🎉');
   };
   return (
-    <div className="body">
-      <Logo />
-      <header className="">
-        <h1 className="title">Factually</h1>
-        <div className="tagline">
-          <p>This is where we explain what this website does.</p>
-          <p>Maybe even in two lines.</p>
-        </div>
-        <div className="search">
-          <input type="text" className="search-term" placeholder="Type a website url"></input>
-          <button type="submit" className="search-button" onClick={_onClick}>
-            <i className="fa fa-search"></i>
-          </button>
-        </div>
-      </header>
+    <>
+      <div className="body">
+        <Logo />
+        <header className="">
+          <h1 className="title">Factually</h1>
+          <div className="tagline">
+            <p>This is where we explain what this website does.</p>
+            <p>Maybe even in two lines.</p>
+          </div>
+          <div className="search">
+            <input type="text" className="search-term" placeholder="Type a website url"></input>
+            <button type="submit" className="search-button" onClick={_onClick}>
+              <i className="fa fa-search"></i>
+            </button>
+          </div>
+        </header>
 
-      <div></div>
-    </div>
+        <div></div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
