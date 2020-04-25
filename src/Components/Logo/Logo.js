5e7c23eddc5ef1ec392848fae53e3e3f18cae0a2
EVA <edie.blues@gmail.com>
2020-04-25 22:21:04 +0200

    add link to logo

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 function Logo() {
   return (
+    <a href='https://github.com/concrete-wow/concrete-wow'>
     <img
       className=""
       style={{ padding: '2rem', height: '6rem' }}
       src="/icons-images/logodraft1.png"
       alt="logo"
     />
+    </a>
   );
 }
 
